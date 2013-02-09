c4781ae7cd2436518594ff87a49c2c04643d9cc1
danielgm <dan@danielmclaren.com>
2013-02-09 04:03:50 -0800

    Got node selection highlighting working. Tooltips now work with panning / zooming.

--- static/js/custom.constellation.js
+++ static/js/custom.constellation.js
@@ -16,7 +16,7 @@ CustomGraphParser.prototype.parse = function(data) {
 
 	var graph = this.graph;
 	
-	var layoutSize = 4096;
+	var layoutSize = 8196;
 	jQuery.each(data.nodes, function(i, node) {
 		// Incoming values are normalized [0-1].
 		node.x = node.x * layoutSize - layoutSize/2;
@@ -67,12 +67,17 @@ CustomLayout.prototype.viewChanged = function(){
 
 CustomNodeRenderer = function(constellation, nodeId, data) {
 	NodeRenderer.call(this, constellation, nodeId, data);
+	this.state = 'default';
 };
 
 CustomNodeRenderer.prototype = new NodeRenderer();
 CustomNodeRenderer.prototype.constructor = CustomNodeRenderer;
 
 CustomNodeRenderer.prototype.defaultStyles = {};
+
+CustomNodeRenderer.prototype.setState = function(state) {
+	this.state = state;
+};
 
 CustomNodeRenderer.prototype.create = function(){
 	var svg = this.constellation.svg;
@@ -82,7 +87,7 @@ CustomNodeRenderer.prototype.create = function(){
 	this.renderer = {
 		group: group,
 		graphic: svg.circle(group, 0, 0, 5, {
-			'fill': this.data.selected ? '#ff9999' : '#9999ff',
+			'fill': '#9999ff',
 			'stroke': '#666666',
 			'strokeWidth': 1
 		})
@@ -109,7 +114,33 @@ CustomNodeRenderer.prototype.create = function(){
 CustomNodeRenderer.prototype.draw = function() {
 	var svg = this['constellation']['svg'];
 
-	svg.change(this.renderer.graphic, {'fill': this.data.selected ? '#ff9999' : '#9999ff'});
+	var selected = this.state == 'selected';
+	var hasSelection = this.constellation.getSelectedNodeId() != null;
+	var settings = {};
+	switch (this.state) {
+		case 'selected':
+			settings = {
+				'fill': '#ff3333',
+				'stroke': '#666666',
+				'strokeWidth': 2
+			};
+			break;
+		case 'emphasized':
+			settings = {
+				'fill': '#ff9999',
+				'stroke': '#666666',
+				'strokeWidth': 1
+			};
+			break;
+		default:
+			settings = {
+				'fill': hasSelection ? '#eeeeee' : '#9999ff',
+				'stroke': '#666666',
+				'strokeWidth': hasSelection ? 0 : 1
+			};
+			break;
+	}
+	svg.change(this.renderer.graphic, settings);
 	
 	this.position();
 	
@@ -127,3 +158,67 @@ CustomNodeRenderer.prototype.destroy = function() {
 };
 
 
+CustomEdgeRenderer = function(constellation, edgeId, tailNodeRenderer, headNodeRenderer, data) {
+	EdgeRenderer.call(this, constellation, edgeId, tailNodeRenderer, headNodeRenderer, data);
+};
+
+CustomEdgeRenderer.prototype = new EdgeRenderer();
+CustomEdgeRenderer.prototype.constructor = CustomEdgeRenderer;
+
+CustomEdgeRenderer.prototype['defaultStyles'] = {};
+
+CustomEdgeRenderer.prototype.setState = function(state) {
+	this.state = state;
+};
+
+CustomEdgeRenderer.prototype.create = function() {
+	var svg = this['constellation']['svg'];
+	var container = this['constellation'].getEdgeContainer();
+	var group = svg.group(container);
+	this.renderer = {
+		group: group,
+		line: svg.line(group, 0, 0, 10, 0, {
+			'display': 'none',
+			'stroke': '#cccccc',
+			'strokeWidth': 2
+		})
+	};
+	
+	jQuery(this.renderer.line)
+		.bind('mouseover', {'context':this}, function(event) {
+			event.data.context['constellation']['edgemouseoverHandler'](event, event.data.context);
+		})
+		.bind('mouseout', {'context':this}, function(event) {
+			event.data.context['constellation']['edgemouseoutHandler'](event, event.data.context);
+		})
+		.bind('mousedown', {'context':this}, function(event) {
+			event.data.context['constellation']['edgemousedownHandler'](event, event.data.context);
+		})
+		.bind('mouseup', {'context':this}, function(event) {
+			event.data.context['constellation']['edgemouseupHandler'](event, event.data.context);
+		})
+		.bind('click', {'context':this}, function(event) {
+			event.data.context['constellation']['edgeclickHandler'](event, event.data.context);
+		})
+		.bind('touchstart', {'context':this}, function(event) {
+			event.data.context['constellation']['edgetouchstartHandler'](event, event.data.context);
+		})
+		.bind('touchend', {'context':this}, function(event) {
+			event.data.context['constellation']['edgetouchendHandler'](event, event.data.context);
+		});
+};
+
+CustomEdgeRenderer.prototype.draw = function() {
+	var hasSelection = this.constellation.getSelectedNodeId() != null;
+	jQuery(this.renderer.line)
+		.attr('x1', this['tailNode']['x'])
+		.attr('y1', this['tailNode']['y'])
+		.attr('x2', this['headNode']['x'])
+		.attr('y2', this['headNode']['y'])
+		.css('stroke', this.state == 'emphasized' ? '#888888' : hasSelection ? '#eeeeee' : '#cccccc')
+		.css('display', 'inline');
+};
+
+CustomEdgeRenderer.prototype.destroy = function() {
+	jQuery(this.renderer.line).remove();
+};
