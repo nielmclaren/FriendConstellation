d9782d338d8865a301c5c2ae84ea7995d8e8f256
danielgm <dan@danielmclaren.com>
2013-02-09 01:44:54 -0800

    Cleanup.

--- static/js/custom.constellation.js
+++ static/js/custom.constellation.js
@@ -1,10 +1,4 @@
 
-/**
- * 
- * @param config
- * @returns {CustomGraphParser}
- * @constructor
- */
 CustomGraphParser = function(config) {
 	// Constructor with no arguments is used for subclasses.
 	if (arguments.length <= 0) return;
@@ -71,14 +65,6 @@ CustomLayout.prototype.viewChanged = function(){
 };
 
 
-/**
- * 
- * @param constellation
- * @param nodeId
- * @param data
- * @returns {CustomNodeRenderer}
- * @constructor
- */
 CustomNodeRenderer = function(constellation, nodeId, data) {
 	NodeRenderer.call(this, constellation, nodeId, data);
 };
@@ -99,25 +85,6 @@ CustomNodeRenderer.prototype.create = function(){
 			'fill': this.data.selected ? '#ff9999' : '#9999ff',
 			'stroke': '#666666',
 			'strokeWidth': 1
-/*
-		}),
-		labelBackground: svg.rect(group, 0, 0, 0, 0, 2, 2, {
-			'fill': '#f6f6f6',
-			'stroke': '#333333',
-			'strokeWidth': 1
-		}),
-		label: svg.text(group, 0, 0, this.data.name, {
-			'style': '-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-o-user-select: none;user-select: none;',
-			'fontFamily': 'Verdana',
-			'fontSize': 10,
-			'fontWeight': 'normal',
-			'fill': '#441111',
-			'textAnchor': 'middle',
-			
-			// HACK: Better cross-browser compatibility with 'dy'
-			//dominantBaseline: 'central'
-			'dy': '.35em'
-*/
 		})
 	};
 	
@@ -141,22 +108,7 @@ CustomNodeRenderer.prototype.create = function(){
 
 CustomNodeRenderer.prototype.draw = function() {
 	var svg = this['constellation']['svg'];
-/*
-	var labelBounds = this.renderer.label.getBBox();
-	var horizontalPadding = 8, verticalPadding = 3;
-	
-	var labelBackground = jQuery(this.renderer.labelBackground);
-	if (labelBounds.width > 0 && labelBounds.height > 0) {
-		labelBackground.css('display', 'inline');
-		labelBackground.attr('x', labelBounds.x - horizontalPadding);
-		labelBackground.attr('y', labelBounds.y - verticalPadding);
-		labelBackground.attr('width', labelBounds.width + 2*horizontalPadding);
-		labelBackground.attr('height', labelBounds.height + 2*verticalPadding);
-	}
-	else {
-		labelBackground.css('display', 'none');
-	}
-*/
+
 	svg.change(this.renderer.graphic, {'fill': this.data.selected ? '#ff9999' : '#9999ff'});
 	
 	this.position();
